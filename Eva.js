7d7c361ce33fcf878be1bcd583671101e5c219fe
Irwin <rodriguez.irwin@gmail.com>
2021-07-20 14:48:24 +0200

    user-defined functions and closures

--- Eva.js
+++ Eva.js
@@ -73,6 +73,20 @@ class Eva {
         }
 
         // -----------------------------------------
+        // Function declaration: (def square (x) (* x x))
+        if (exp[0] === 'def') {
+            const [_tag, name, params, body] = exp;
+
+            const fn = {
+                params,
+                body,
+                env, // this this the secret for the Closure support!
+            };
+
+            return env.define(name, fn);
+        }
+
+        // -----------------------------------------
         // Function calls:
         //
         // (print "Hello World")
@@ -88,16 +102,35 @@ class Eva {
             if (typeof fn === 'function') {
                 return fn(...args);
             }
+
             // 2. User-defined function:
-            // TODO
+            const activationRecord = {}; // Dictionary
+            fn.params.forEach((param, index) => {
+                activationRecord[param] = args[index];
+            });
+
+            const activationEnv = new Environment(
+                activationRecord,
+                fn.env);
+
+            return this._evalBody(fn.body, activationEnv);
         }
         
-
-        
-
-
         throw `Unimplemented: ${JSON.stringify(exp)}`;
     }
+
+    /**
+     * Function body evaluation
+     * This is neccesary because Eva creates a fresh environment in blocks
+     * and functions already has their own fresh environments.
+     */
+    _evalBody(body, env) {
+        if (body[0] === 'begin') {
+            return this._evalBlock(body, env);
+        }
+        return this.eval(body, env);
+    }
+
     /**
      * Block evaluation
      */
--- __tests__/run.js
+++ __tests__/run.js
@@ -3,13 +3,14 @@ const Environment = require('../Environment');
 const { test } = require('./test-util');
 
 const tests = [
-    // require('./self-eval-test.js'),
-    // require('./math-test.js'),
-    // require('./variable-test.js'),
-    // require('./block-test.js'),
-    // require('./if-test.js'),
-    // require('./while-test.js'),
+    require('./self-eval-test.js'),
+    require('./math-test.js'),
+    require('./variable-test.js'),
+    require('./block-test.js'),
+    require('./if-test.js'),
+    require('./while-test.js'),
     require('./built-in-function-test'),
+    require('./user-defined-function-test'),
 ];
 
 // ----------------------------------------
