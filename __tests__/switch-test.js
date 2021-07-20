7ec1ffc70b788a7b7175053a1cc8a61925c54638
Irwin <rodriguez.irwin@gmail.com>
2021-07-20 22:50:45 +0200

    switch expression and some homework.

---
+++ __tests__/switch-test.js
@@ -0,0 +1,15 @@
+const {test} = require('./test-util');
+
+module.exports = (eva) => {
+    test(eva, `
+    (begin
+        (var x 10)
+        (switch 
+            ((= x 10) 100)
+            ((> x 10) 200)
+            (else     300)
+        )
+    )
+    `, 100);
+    
+};
